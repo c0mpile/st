0952d66bbe08b2631d29eb346a05e5e8af101616
Kevin <kevin@chroot.us>
2025-03-17 16:01:35 -0400

    font change

--- config.def.h
+++ config.def.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrainsMono Nerd Font :pixelsize=15:antialias=true:autohint=true";
-static char *font2[] = { "JetBrainsMono Nerd Font :pixelsize=15:antialias=true:autohint=true" };
+static char *font = "LiterationMono Nerd Font :pixelsize=15:antialias=true:autohint=true";
+static char *font2[] = { "LiterationMono Nerd Font :pixelsize=15:antialias=true:autohint=true" };
 static int borderpx = 0;
 
 /*
@@ -17,7 +17,7 @@ static int borderpx = 0;
  * 4: value of shell in /etc/passwd
  * 5: value of shell in config.h
  */
-static char *shell = "/bin/sh";
+static char *shell = "/bin/zsh";
 char *utmp = NULL;
 char *stty_args = "stty raw pass8 nl -echo -iexten -cstopb 38400";
 
